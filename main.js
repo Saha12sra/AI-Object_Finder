8309691f1b2de529d2f3cc772d017bf63a5cb333
Saha12sra <74057039+Saha12sra@users.noreply.github.com>
2022-01-17 14:51:43 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ status = "";
 
 function setup() {
 
-    canvas = createCanvas(420, 380);
+    canvas = createCanvas(420, 345);
     canvas.center();
     video = createCapture(VIDEO);
-    video.size(420, 380);
+    video.size(420, 345);
     video.hide();
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
     document.getElementById("status").innerHTML = "Status: Detecting Objects";
 }
 function draw() {
 
-    image(video, 0, 0, 420, 380);
+    image(video, 0, 0, 420, 345);
 
 }
 
